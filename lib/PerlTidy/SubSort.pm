ac39de5c15d1faefd7e2ed0b795063eb0c641416
Andrew Speer <andrew.speer@isolutions.com.au>
2014-12-31 17:08:30 +1030

    MANIFEST Import

---
+++ lib/PerlTidy/SubSort.pm
@@ -0,0 +1,139 @@
+#!/usr/bin/env perl
+
+#  This file is part of PerlTidy::SubSort.
+#
+#  This software is copyright (c) 2014 by Andrew Speer <andrew.speer@isolutions.com.au>.
+#
+#  This is free software; you can redistribute it and/or modify it under
+#  the same terms as the Perl 5 programming language system itself.
+#
+#  Full license text is available at:
+#
+#  <http://dev.perl.org/licenses/>
+#
+
+
+#  Package to sort subroutines within a perl file
+#
+package PerlTidy::SubSort;
+
+
+#  Pragma
+#
+use strict;
+use vars qw($VERSION @EXPORT_OK);
+use warnings;
+
+
+#  Export functions
+#
+use base 'Exporter';
+@EXPORT_OK=qw(subsort);
+
+
+#  External Modules
+#
+use PPI;
+use PerlTidy::SubSort::Util;
+
+
+#  Version
+#
+$VERSION='0.004';
+
+
+#  Done
+#
+1;
+
+
+#==================================================================================================
+
+
+sub subsort {
+
+
+    #  Get document file name as argument
+    #
+    my $ppi_fn=shift() ||
+        return err('no filename supplied');
+    my $ppi_or = PPI::Document->new($ppi_fn) ||
+        return err("can't open $ppi_fn, $!");
+
+
+    #  Hoover up subroutines into an array by order and hash by name
+    #
+    my (%sub_public, @sub_public);
+    my (%sub_private, @sub_private);
+    SUB: foreach my $sub ( @{ $ppi_or->find('PPI::Statement::Sub') || [] } ) {
+        #  Ignore BEGIN etc.
+        next if (ref($sub) eq 'PPI::Statement::Scheduled');
+        #  Ignore subs with # no subsort pragra
+        foreach my $comment ( @{ $sub->find('PPI::Token::Comment') || [] } ) {
+            next SUB if ($comment->content)=~/no\s+subsort/
+        }
+        unless ( $sub->forward ) {
+            if ((my $sub_name=$sub->name)=~/^_/) {
+                push @sub_private, $sub;
+                $sub_private{$sub_name}=$sub;
+            }
+            else {
+                push @sub_public, $sub;
+                $sub_public{$sub_name}=$sub;
+            }
+        }
+    }
+
+
+    #  Now sort by alpha name for public routines
+    #
+    my @sub_public_sort;
+    foreach my $sub_name (sort {$a cmp $b} keys %sub_public) {
+        push @sub_public_sort, $sub_public{$sub_name}->clone();
+    }
+    foreach my $i (0..$#sub_public_sort) {
+        my ($sub_remove, $sub_insert)=($sub_public[$i], $sub_public_sort[$i]);
+        $sub_remove->insert_before($sub_insert);
+        $sub_remove->delete();
+        
+    }
+
+
+    #  Now sort by alpha name for private
+    #
+    my @sub_private_sort;
+    foreach my $sub_name (sort {$a cmp $b} keys %sub_private) {
+        push @sub_private_sort, $sub_private{$sub_name}->clone();
+    }
+    foreach my $i (0..$#sub_private_sort) {
+        my ($sub_remove, $sub_insert)=($sub_private[$i], $sub_private_sort[$i]);
+        $sub_remove->insert_before($sub_insert);
+        $sub_remove->delete();
+        
+    }
+
+
+    #  All done - save
+    #
+    $ppi_or->save($ppi_fn);
+    
+    
+    #  Return number of routines sorted
+    #
+    my $count=@sub_public + @sub_private;
+    return \$count;
+
+}
+
+__END__
+
+=head LICENSE
+
+This software is copyright (c) 2014 by Andrew Speer <andrew.speer@isolutions.com.au>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+Full license text is available at:
+
+<http://dev.perl.org/licenses/>
---
+++ lib/PerlTidy/SubSort/Util.pm
@@ -0,0 +1,83 @@
+#  This file is part of PerlTidy::SubSort.
+#
+#  This software is copyright (c) 2014 by Andrew Speer <andrew.speer@isolutions.com.au>.
+#
+#  This is free software; you can redistribute it and/or modify it under
+#  the same terms as the Perl 5 programming language system itself.
+#
+#  Full license text is available at:
+#
+#  <http://dev.perl.org/licenses/>
+#
+package PerlTidy::SubSort::Util;
+
+
+#  Pragma
+#
+use strict;
+use vars qw($VERSION @EXPORT);
+use warnings;
+no warnings qw(uninitialized);
+
+
+#  External modules
+#
+use Carp;
+
+
+#  Export functions
+#
+use base 'Exporter';
+@EXPORT=qw(err msg arg);
+
+
+#  Version information in a format suitable for CPAN etc. Must be
+#  all on one line
+#
+$VERSION='0.004';
+
+
+#  Done
+#
+1;
+
+#==================================================================================================
+
+
+sub err {
+
+
+    #  Quit on errors
+    #
+    my $msg=shift();
+    croak &fmt("*error*\n\n" . ucfirst($msg), @_);
+
+}
+
+
+sub fmt {
+
+
+    #  Format message nicely. Always called by err or msg so caller=2
+    #
+    my $message=sprintf(shift(), @_);
+    chomp($message);
+    my $caller=(split(/:/, (caller(2))[3]))[-1];
+    $caller=~s/^_?!(_)//;
+    my $format=' @<<<<<<<<<<<<<<<<<<<<<< @<';
+    formline $format, $caller . ':', '';
+    $message=$^A . $message; $^A=undef;
+    return $message;
+
+}
+
+
+sub msg {
+
+
+    #  Print message
+    #
+    CORE::print &fmt(@_), "\n";
+
+}
+
